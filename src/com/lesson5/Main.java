8ea9efc5e8424b1189e0875800626edb4be3755e
TroninDmitry <dima-tronin@mail.ru>
2020-12-27 21:53:48 +0300

    Сделал ДЗ
    E-mail, решил сделать автогенерирующимся, на всякий случай добавил id сотрудника

--- src/com/lesson5/Main.java
+++ src/com/lesson5/Main.java
@@ -1,8 +1,33 @@
 package com.lesson5;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Person [] personArray= new Person[5];
+        getArrayPerson(personArray);
+        getPersonWhoOlderByValue(personArray);
+    }
+
+    private static void getArrayPerson(Person[] personArray){
+
+        personArray[0]= new Person("Ivanov Invan","Sales manager","+79000000001",30000f, 25);
+        personArray[1]= new Person("Petrov Petr","CEO","+79000000001",80000f,42);
+        personArray[2]= new Person("Sezhakova Bella","Cleaning manager","+79666666661",2500.3f,39);
+        personArray[3]= new Person("Sidorov Sidor", "accountant","+79000000023", 25000.3f,41);
+        personArray[4]= new Person("Putin Vladimir", "Security officer","+79990001001",200000.99f,60);
+
+    }
+
+    private static void getPersonWhoOlderByValue(Person[] personArray){
+        System.out.println("Введите возраст:");
+        Scanner scanner= new Scanner(System.in);
+        int value =scanner.nextInt();
+        System.out.println("Будут показаны сотрудники старше "+value);
+        for (int i = 0; i <personArray.length ; i++) {
+            if (personArray[i].getAge()>value) personArray[i].outPersonToConsole();
+        }
     }
 }
--- src/com/lesson5/Person.java
+++ src/com/lesson5/Person.java
@@ -1,5 +1,100 @@
 package com.lesson5;
 
 public class Person {
-    
+    private static int id;
+    private String fullName, position, eMail, phoneNumber;
+    private float salary;
+    private int age;
+    private int personId;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    Person(String fullName, String position, String phoneNumber, float salary, int age ){
+        personId=id++;
+        this.fullName=fullName;
+        this.position=position;
+        this.eMail=eMail;
+        this.phoneNumber=phoneNumber;
+        this.salary=salary;
+        this.age=age;
+        this.eMail= createEMail(fullName);
+    }
+
+    private String createEMail(String fullName){
+        String surname = "";
+        for (int i = 0; i < fullName.indexOf(' '); i++) {
+            surname=surname+fullName.charAt(i);
+        }
+        return fullName.toLowerCase().charAt(fullName.indexOf(' ')+1)+"."+surname.toLowerCase()+"@example.com";
+
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник{" +
+                "ФИО='" + fullName + '\'' +
+                ", Должность='" + position + '\'' +
+                ", e-mail='" + eMail + '\'' +
+                ", Номер телефона='" + phoneNumber + '\'' +
+                ", Заработная плата=" + salary +
+                ", Возраст=" + age +
+                '}';
+    }
+    public void outPersonToConsole(){
+        System.out.println(toString());
+    }
 }
